a88c7ecd9fc558d9766d03caa304776bcc55f435
Aadhi Senthil Kumar <aadhi.senthilkumar06@gmail.com>
2026-05-07 10:35:39 +1000

    added placeholder icons for projects

--- asen_portfolio/src/App.tsx
+++ asen_portfolio/src/App.tsx
@@ -1,13 +1,25 @@
 import './App.css'
 
+const projects = ["Project 1", "Project 2", "Project 3", "Project 4"];
+
 function App() {
   return (
+    <>
   <div className="custom-font">
     Welcome to my little corner <br />
     on the internet :D
-  </div>
+    </div>
+    <div className="icon-container"> 
+    {projects.map((project) => (
+      <div className="project-icons" key={project}> 
+      </div>  
+    ))}
+    </div>
+    </>
   );
-
 }
 
 export default App
+// 1. icons of projects and about me section 
+// 2. overview of who i am 
+// 3. hovering ... animation for reaching out, linkedin, github, x? 
